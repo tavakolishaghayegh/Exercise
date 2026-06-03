1b6cabacef2fc6bb469e622926b3765c63ca3188
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-06-04 02:18:04 +0330

    finish object property exercise

--- box-model/box-model-exercise-1/test/index.js
+++ box-model/box-model-exercise-1/test/index.js
@@ -6,3 +6,22 @@ console.log(typeof value);
 
 const posetive = false;
 console.log(typeof posetive);
+
+const userInfo = {
+  FirstName: "shaghayegh",
+  LastName: "tavakoli",
+  age: 24,
+  isMarried: false,
+};
+console.log(userInfo);
+
+const car = {
+  color: "red",
+  year: "2002",
+  price: 200000,
+  brand: "bmw",
+};
+car.year = 2004;
+const selectedKey = "color";
+car["color"] = "blue";
+console.log(car.color);
